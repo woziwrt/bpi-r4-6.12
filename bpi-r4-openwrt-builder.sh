37897d1c90d2ac4ef69a90d669bfd1b316826f36
Google <156495286+woziwrt@users.noreply.github.com>
2025-10-26 09:49:47 +0100

    Builder update + crypto ext. test_26102025

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -4,53 +4,49 @@ rm -rf openwrt
 rm -rf mtk-openwrt-feeds
 
 git clone --branch master https://github.com/openwrt/openwrt.git openwrt || true
-cd openwrt; git checkout 5edf6a4c25d6e5fc9046d4e5533b79b07bcb3ef4; cd -;		#ath79: whr-g301n: remove custon wifi LED
+cd openwrt; git checkout e112fd8e59e37ae323fdaebb74bdd6084176d8e4; cd -;		#mt76: scripts/feeds: implement support for --root option
 
 git clone https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds || true
-cd mtk-openwrt-feeds; git 89f9cb071fe99ffdd4d08b70bd22ef03f68481d8; cd -;	#[kernel-6.12][common][spim-nand][Change full ubi version dtso naming]
+cd mtk-openwrt-feeds; git checkout 718ab32d40c2e82fadc882c035e665a79a14c082; cd -;	#[openwrt-master][MAC80211][wed][Fix wed patch fail]
 
-echo "89f9cb" > mtk-openwrt-feeds/autobuild/unified/feed_revision
+echo "c095e1" > mtk-openwrt-feeds/autobuild/unified/feed_revision
 
 #\cp -r my_files/w-rules mtk-openwrt-feeds/autobuild/unified/filogic/rules
 \cp -r my_files/w-unified_rules mtk-openwrt-feeds/autobuild/unified/rules
 
-rm -rf openwrt/package/network/utils/iw/patches/*
-rm -rf openwrt/package/network/services/hostapd/patches/*
-rm -rf openwrt/package/network/services/hostapd/src
-rm -rf openwrt/package/kernel/mac80211/patches/*
-rm -rf openwrt/package/kernel/mt76/src/*
-rm -rf openwrt/package/network/config/wifi-scripts/files
+\cp -r my_files/w-remove_list.txt mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/master/remove_list.txt 
+#\cp -r configs/basic.defconfig mtk-openwrt-feeds/autobuild/unified/filogic/master/defconfig
 
-#\cp -r my_files/0001-1-wozi-trace.h-strlcpy-strscpy-replacement.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/kernel/mt76/patches/
-\cp -r my_files/0011-wozi-mtk-mt76-add-debug-tools.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/kernel/mt76/patches/0011-mtk-mt76-add-debug-tools.patch
-
-\cp -a mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/network/services/hostapd/patches/. openwrt/package/network/services/hostapd/patches/ 
-\cp -r mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/network/services/hostapd/files/. openwrt/package/network/services/hostapd/files/
-\cp -a mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/network/utils/iw/patches/. openwrt/package/network/utils/iw/patches/
-\cp -a mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/kernel/mac80211/patches/. openwrt/package/kernel/mac80211/patches/
-\cp -a mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/kernel/mt76/src/. openwrt/package/kernel/mt76/src/
-\cp -a mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/kernel/mt76/patches openwrt/package/kernel/mt76/patches
-\cp -a mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/network/config/wifi-scripts/files openwrt/package/network/config/wifi-scripts/files
-#\cp -a mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/network/config/netifd/patches openwrt/package/network/config/netifd/patches
-
-
-### required & thermal zone 
-\cp -r my_files/1007-wozi-arch-arm64-dts-mt7988a-add-thermal-zone.patch mtk-openwrt-feeds/24.10/patches-base/
-
-sed -i 's/CONFIG_PACKAGE_perf=y/# CONFIG_PACKAGE_perf is not set/' mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/defconfig
-sed -i 's/CONFIG_PACKAGE_perf=y/# CONFIG_PACKAGE_perf is not set/' mtk-openwrt-feeds/autobuild/autobuild_5.4_mac80211_release/mt7988_wifi7_mac80211_mlo/.config
-sed -i 's/CONFIG_PACKAGE_perf=y/# CONFIG_PACKAGE_perf is not set/' mtk-openwrt-feeds/autobuild/autobuild_5.4_mac80211_release/mt7986_mac80211/.config
-
-#\cp -r my_files/3703-wozi- 6.12.45-remove-uci-duplicate-ports.patch mtk-openwrt-feeds/autobuild/unified/filogic/master/patches-base
+\cp -r my_files/wozi_files/. mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/master/files/
 
 cd openwrt
-bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt7988_rfb-mt7996 log_file=make
+bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt7988_rfb-mt7996 prepare log_file=prepare
+
+bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt7988_rfb-mt7996 build log_file=build
+
+bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt7988_rfb-mt7996 release log_file=release
 
 exit 0
 
+============================ extension for Telit FN990 family
+
 cd openwrt
-\cp -r ../configs/6.12.config .config
+\cp -r ../my_files/sms-tool/ feeds/packages/utils/sms-tool
+\cp -r ../my_files/modemdata-main/ feeds/packages/utils/modemdata 
+\cp -r ../my_files/luci-app-modemdata-main/luci-app-modemdata/ feeds/luci/applications
+\cp -r ../my_files/luci-app-lite-watchdog/ feeds/luci/applications
+\cp -r ../my_files/luci-app-sms-tool-js-main/luci-app-sms-tool-js/ feeds/luci/applications
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+\cp -r ../my_files/qmi.sh package/network/utils/uqmi/files/lib/netifd/proto/
+chmod -R 755 package/network/utils/uqmi/files/lib/netifd/proto
+chmod -R 755 feeds/luci/applications/luci-app-modemdata/root
+chmod -R 755 feeds/luci/applications/luci-app-sms-tool-js/root
+chmod -R 755 feeds/packages/utils/modemdata/files/usr/share
+
+\cp -r configs/config.telit.ext .config
+
 make menuconfig
 make -i -j1 V=sc | tee finalmake.txt
-
-
