cfafe55a2ba4cfc5a89b3297abde539c2d158937
Google <156495286+woziwrt@users.noreply.github.com>
2025-10-30 09:12:55 +0100

    basic wifi added + improvement

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -4,31 +4,23 @@ rm -rf openwrt
 rm -rf mtk-openwrt-feeds
 
 git clone --branch master https://github.com/openwrt/openwrt.git openwrt || true
-cd openwrt; git checkout e112fd8e59e37ae323fdaebb74bdd6084176d8e4; cd -;		#mt76: scripts/feeds: implement support for --root option
+cd openwrt; git checkout df950f4cfddd6696fe72f51d4260152f08bd643f; cd -;		#prereq: use staging_dir's compiler
 
 git clone https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds || true
-cd mtk-openwrt-feeds; git checkout 718ab32d40c2e82fadc882c035e665a79a14c082; cd -;	#[openwrt-master][MAC80211][wed][Fix wed patch fail]
+cd mtk-openwrt-feeds; git checkout 3cb1207fb1473880fe6ab9ea85dd50e8781c14ce; cd -;	#[kernel-6.12][common][Enable spidev config]
 
-echo "718ab3" > mtk-openwrt-feeds/autobuild/unified/feed_revision
+#\cp -r my_files/feed_revision mtk-openwrt-feeds/autobuild/unified/
 
 #\cp -r my_files/w-rules mtk-openwrt-feeds/autobuild/unified/filogic/rules
-\cp -r my_files/w-unified_rules mtk-openwrt-feeds/autobuild/unified/rules
-
-\cp -r my_files/w-remove_list.txt mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/master/remove_list.txt 
-#\cp -r configs/basic.defconfig mtk-openwrt-feeds/autobuild/unified/filogic/master/defconfig
-
-\cp -r my_files/wozi_files/. mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/master/files/
+#\cp -r my_files/w-unified_rules mtk-openwrt-feeds/autobuild/unified/rules
 
 cd openwrt
-bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt7988_rfb-mt7996 prepare log_file=prepare
-
-bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt7988_rfb-mt7996 build log_file=build
-
-bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt7988_rfb-mt7996 release log_file=release
+bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic log_file=make
 
 exit 0
 
-\cp -r configs/config.mm.dbg openwrt/.config
+\cp -r configs/config.mm.dbg openwrt/.config		#Modemmanager+other extension
+make -j $(nproc) V=s
 
 ============================ extension for Telit FN990 family
 
@@ -48,7 +40,9 @@ chmod -R 755 feeds/luci/applications/luci-app-modemdata/root
 chmod -R 755 feeds/luci/applications/luci-app-sms-tool-js/root
 chmod -R 755 feeds/packages/utils/modemdata/files/usr/share
 
-\cp -r configs/config.telit.ext .config
+\cp -r ../configs/config.telit.ext .config
+
+scripts/feeds uninstall crypto-eip pce tops-tool
 
 make menuconfig
-make -i -j1 V=sc | tee finalmake.txt
+make -j $(nproc) V=s
