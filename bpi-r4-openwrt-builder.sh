2cf1b78d49368cb3f77f621f5382da1e5fa69ba8
Google <156495286+woziwrt@users.noreply.github.com>
2026-02-06 06:18:17 +0100

    RC4

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -4,10 +4,10 @@ rm -rf openwrt
 rm -rf mtk-openwrt-feeds
 
 git clone --branch openwrt-25.12 https://github.com/openwrt/openwrt.git openwrt || true
-cd openwrt; git checkout beb134292d66d5437b8b99f8fca60976aaf817a0; cd -;		#kernel: fix rtl8261n driver for non-realtek chips
+cd openwrt; git checkout 9bf33c02d5c4dc9cb50bfaa2c23bb4398a99c49d; cd -;		#kernel: bump 6.12 to 6.12.68
 
 git clone --branch master https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds || true
-cd mtk-openwrt-feeds; git checkout fd0741fc9d8c18b540a5a3192e059cd546011ee2; cd -;	#[kernel-6.12][mt7988][crypto][Add txd info for IPSec inline mode in gro frag_list processing]
+cd mtk-openwrt-feeds; git checkout 8a91c67871bc747187aedc169867378e8564f007; cd -;	#[uboot][common][bsp][upgrade to mtk-openwrt/u-boot 2026-01-23 release]
 
 #\cp -r my_files/feed_revision mtk-openwrt-feeds/autobuild/unified/
 
@@ -19,7 +19,7 @@ cd mtk-openwrt-feeds; git checkout fd0741fc9d8c18b540a5a3192e059cd546011ee2; cd 
 \cp -r my_files/w-Makefile openwrt/package/libs/musl-fts/Makefile
 
 ### tx_power patch - required for BE14 boards with defective eeprom flash
-mkdir -p openwrt/package/kernel/mt76/patches && cp -r my_files/99999_tx_power_check.patch $_
+\cp -r my_files/99999_tx_power_check.patch openwrt/package/kernel/mt76/patches
 
 cd openwrt
 bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt798x_rfb-wifi7_nic log_file=make
@@ -29,11 +29,11 @@ exit 0
 cd openwrt
 \cp -r ../my_files/w-filogic.mk target/linux/mediatek/image/filogic.mk
 #\cp -r ../configs/mtk_test.nocrypto.config openwrt/.config
-\cp -r ../configs/mtk_test.crypto.config openwrt/.config
-make menuconfig
-make -j$(nproc) V=s
+#\cp -r ../configs/mtk_test.crypto.config openwrt/.config
+#make menuconfig
+#make -j$(nproc) V=s
 
-exit 0
+#exit 0
 
 
 #============================ extension for Telit FN990 family
