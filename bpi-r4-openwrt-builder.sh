faa805ed730425faed3ed42bf4ba1d414b8a4545
Petr Wozniak <petr.wozniak@gmail.com>
2026-03-15 12:26:35 +0100

    not tested but added Ivan Mironov's BE14 eeprom patch

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -16,8 +16,8 @@ cd mtk-openwrt-feeds; git checkout 6ce60af4713fffc97222136bc94a5fcf858ef409; cd 
 
 \cp -r my_files/9999-image-bpi-r4-sdcard.patch mtk-openwrt-feeds/25.12/patches-base
 
-### tx_power check Gilly_1970's patch - for defective BE14 boards with defective eeprom flash
-#\cp -r my_files/0140-wifi-mt76-mt7996-use-mt76_get_txpower_cur.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/25.12/files/package/kernel/mt76/patches
+### tx_power check Ivan Mironov's patch - for defective BE14 boards with defective eeprom flash
+\cp -r my_files/100-wifi-mt76-mt7996-Use-tx_power-from-default-fw-if-EEP.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/25.12/files/package/kernel/mt76/patches
 
 cd openwrt
 bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt798x_rfb-wifi7_nic prepare
